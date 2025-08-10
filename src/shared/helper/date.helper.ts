06c802ad6888e922c96d72735d53535896832b95
rucci <d.rucci.2001@gmail.com>
2025-08-10 17:07:10 +0700

    Template from Auth

---
+++ src/shared/helper/date.helper.ts
@@ -0,0 +1,12 @@
+import { DateTime } from 'luxon';
+import { TIMEZONE } from '../constant/global.constant';
+
+export class DateHelper {
+  static now(): DateTime {
+    return DateTime.now().setZone(TIMEZONE);
+  }
+
+  static nowDate(): Date {
+    return this.now().toJSDate();
+  }
+}
